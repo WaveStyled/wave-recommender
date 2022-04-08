4eed3b6a6463bd9bb3692229354f68a53bbb5a7a
jaysan6 <sanjay.shrikanth@gmail.com>
2022-04-08 08:01:22 -0700

    Item updated

--- Item.py
+++ Item.py
@@ -8,43 +8,22 @@
 class Item:
     attribute_mappings = {"pieceid": 0,
                             "color": 1,
-                            "type": 4,
-                            "recent_date_worn": 5,
-                            "times_worn": 6,
-                            "rating": 7,
-                            "oc_formal": 8,
-                            "oc_semi_formal": 9,
-                            "oc_casual": 10,
-                            "oc_workout": 11,
-                            "oc_outdoors": 12,
-                            "oc_comfy": 13,
-                            "we_cold": 14,
-                            "we_hot": 15,
-                            "we_rainy": 16,
-                            "we_snowy": 17,
-                            "we_avg_tmp": 18,
-                            "dirty": 19}
-
-
-                            pieceID INT PRIMARY KEY, \
-        COLOR VARCHAR(12), \
-        TYPE VARCHAR(5), \
-        RECENT_DATE_WORN DATE, \
-        TIMES_WORN INT, \
-        RATING NUMERIC(3,2) DEFAULT 0.50, \
-        OC_FORMAL BOOLEAN, \
-        OC_SEMI_FORMAL BOOLEAN, \
-        OC_CASUAL BOOLEAN, \
-        OC_WORKOUT BOOLEAN, \
-        OC_OUTDOORS BOOLEAN, \
-        OC_COMFY BOOLEAN, \
-        WE_COLD BOOLEAN, \
-        WE_HOT BOOLEAN, \
-        WE_RAINY BOOLEAN, \
-        WE_SNOWY BOOLEAN, \
-        WE_AVG_TMP BOOLEAN, \
-        DIRTY BOOLEAN
-
+                            "type": 2,
+                            "recent_date_worn": 3,
+                            "times_worn": 4,
+                            "rating": 5,
+                            "oc_formal": 6,
+                            "oc_semi_formal": 7,
+                            "oc_casual": 8,
+                            "oc_workout": 9,
+                            "oc_outdoors": 10,
+                            "oc_comfy": 11,
+                            "we_cold": 12,
+                            "we_hot": 13,
+                            "we_rainy": 14,
+                            "we_snowy": 15,
+                            "we_typical": 16,
+                            "dirty": 17 }
     def __init__ (self, row_tuple):
         self.data = tuple(row_tuple)
 
@@ -54,14 +33,14 @@ class Item:
     def getPK(self):
         return self.data[0]
 
-    def getColorCodes(self):
-        return (self.data[1], self.data[2], self.data[3])
+    def getColorStr(self):
+        return self.data[1]
 
     def getOCs(self):
-        return tuple([self.data[i] for i in range(8,14)])
+        return tuple([self.data[i] for i in range(6,12)])
 
     def getWEs(self):
-        return tuple([self.data[i] for i in range(14,19)])
+        return tuple([self.data[i] for i in range(12,17)])
 
     def __str__ (self):
         return str(self.data)
