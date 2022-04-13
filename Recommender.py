763a946aa713e987cf3b4a61f9fcc7321f20fa20
jaysan6 <sanjay.shrikanth@gmail.com>
2022-04-12 17:25:07 -0700

    done

--- Recommender.py
+++ Recommender.py
@@ -7,33 +7,52 @@ from Wardrobe import Wardrobe
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
-from tensorflow.keras.models import Model, Sequential
-from tensorflow.keras.layers import Dense, Input, Activation
-from tensorflow.keras.optimizers import SGD
-from tensorflow.keras.callbacks import Callback
-from sklearn.model_selection import train_test_split
+#from tensorflow.keras.models import Model, Sequential
+#from tensorflow.keras.layers import Dense, Input, Activation
+#from tensorflow.keras.optimizers import SGD
+#from tensorflow.keras.callbacks import Callback
+#from sklearn.model_selection import train_test_split
 
 class Recommender(Wardrobe):
 
     colors = {}
 
     def __init__ (self):
-        super().__init__(['outfit_id','hat','shirt','sweater','jacket','bottom_layer',
-                'shoes','misc','times_worn','recent_date_worn','fit_score','occasion','weather','liked'])
-
+         super().__init__(['outfit_id','hat','shirt','sweater','jacket','bottom_layer',
+                 'shoes','misc','times_worn','recent_date_worn','fit_score','occasion','weather','liked'])
+    
     def normalize(self, col=['hat','shirt','sweater','jacket','bottom_layer','shoes','misc']):
-        for c in col:
-            self.dt[c] = self.dt[c] / self.dt[c].abs().max()
-            self.dt[c].fillna(0, inplace=True)
+         for c in col:
+             self.dt[c] = self.dt[c] / self.dt[c].abs().max()
+             self.dt[c].fillna(0, inplace=True)
+ 
+    def create_train(self, wd): # return 107 16-tuples
+        train = []
+        for of in self.dt.itertuples():
+            outfit = [of.hat, of.shirt, of.sweater, of.jacket, of.bottom_layer, of.shoes, of.misc]
+            colors = [of.color_hat, of.color_shirt, of.color_sweater, of.color_jacket, of.color_bottom_layer, of.color_shoes, of.color_misc]
+            liked = [1 if of.liked == 't' else 0]
+            train.append(np.array(outfit+colors+liked))
+        return train
+    
+    def addColors(self, wd):
+        self.dt['color_hat'] = self.dt.apply(
+                lambda row : wd.getItem(row.hat)[2] if wd.getItem(row.hat) else None, axis=1)
+        self.dt['color_shirt'] = self.dt.apply(
+                lambda row : wd.getItem(row.shirt)[2] if wd.getItem(row.shirt) else None, axis=1)
+        self.dt['color_sweater'] = self.dt.apply(
+                lambda row : wd.getItem(row.sweater)[2] if wd.getItem(row.sweater) else None, axis=1)
+        self.dt['color_jacket'] = self.dt.apply(
+                lambda row : wd.getItem(row.jacket)[2] if wd.getItem(row.jacket) else None, axis=1)
+        self.dt['color_bottom_layer'] = self.dt.apply(
+                lambda row : wd.getItem(row.bottom_layer)[2] if wd.getItem(row.bottom_layer) else None, axis=1)
+        self.dt['color_shoes'] = self.dt.apply(
+                lambda row : wd.getItem(row.shoes)[2] if wd.getItem(row.shoes) else None, axis=1)
+        self.dt['color_misc'] = self.dt.apply(
+                lambda row : wd.getItem(row.misc)[2] if wd.getItem(row.misc) else None, axis=1)
 
     def getdf(self):
         return self.dt
-
-    def revert(self):
-        pass
-    
-    def copy(self):
-        return self.dt.copy()
 
     def train(self):
 
@@ -84,14 +103,23 @@ def add_color_to_mapping(mapping, color):
 
 
 def main():
+    w = Wardrobe()
+    w.from_csv("./good_matts_wardrobe.csv")
+    print(w.getItem(146))
     r = Recommender()
     r.from_csv('./outfits.csv')
     print(r)
+    r.from_csv('./outfits_small.csv')
+    r.addColors(w)
+    # print(r)
     r.normalize()
     print(r.getdf().head())
-
+    #print(r.getdf().head())
+    #print(r.getdf().head())
+    print(r.create_train(w))
+ 
 if __name__ == '__main__':
-    main()
+     main()
 
 
 #tf.debugging.set_log_device_placement(True)
